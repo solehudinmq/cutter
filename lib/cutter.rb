c43745b56e7369e589f44f65fe92067398d884f7
SolehMQ <solehudinmq@gmail.com>
2025-08-29 21:27:04 +0700

    first commit

---
+++ lib/cutter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cutter/version"
+
+module Cutter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cutter_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cutter do
+  it "has a version number" do
+    expect(Cutter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
