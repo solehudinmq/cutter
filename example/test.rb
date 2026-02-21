c2da8679a6841944bc4070a4bb4e728d4f15dbbd
SolehMQ <solehudinmq@gmail.com>
2026-02-21 16:52:24 +0700

    remove unused file

--- example/test.rb
+++ example/test.rb
@@ -1,12 +1,12 @@
 require 'cutter'
 
-require_relative 'models/product'
+puts "====================================================="
+puts "1. case of targeted API performance in good condition"
+puts "====================================================="
 
-Product.delete_all
-
-puts "======================== STARTED [ case of targeted API performance in good condition ] ========================"
 cb = ::Cutter::CircuitBreaker.new(threshold: 2, timeout: 1)
 
+datas = []
 5.times do |i|
   id = i + 1
   begin
@@ -14,12 +14,7 @@ cb = ::Cutter::CircuitBreaker.new(threshold: 2, timeout: 1)
     puts "Request #{i+1} successful, with status code #{result.code}."
     
     payload = JSON.parse(result.body)
-    product = Product.new(title: payload["title"], description: payload["description"], category: payload["category"], price: payload["price"])
-    if product.save
-      puts "Product with id #{product.id} successfully created"
-    else
-      puts "Product with request #{i+1} failed to created"
-    end
+    datas << { id: payload["id"], title: payload["title"], description: payload["description"], category: payload["category"], price: payload["price"] }
   rescue => e
     puts "Request #{i+1} failed, with message #{e.message}."
   end
@@ -28,19 +23,17 @@ cb = ::Cutter::CircuitBreaker.new(threshold: 2, timeout: 1)
 end
 
 puts "\n"
-puts "============= [ all product data ] ================="
-puts "#{Product.all.to_a}"
-puts "============= [ all product data ] ================="
+puts "============= [ data that was successfully obtained ] ================="
+puts "#{datas}"
 puts "\n"
 
-puts "======================== ENDED [ case of targeted API performance in good condition ] ========================"
-puts "\n"
+puts "============================================================="
+puts "2. case of targeted API performance in problematic conditions"
+puts "============================================================="
 
-Product.delete_all
-
-puts "======================== STARTED [ case of targeted API performance in problematic conditions ] ========================"
 cb = ::Cutter::CircuitBreaker.new(threshold: 2, timeout: 1)
 
+datas = []
 7.times do |i|
   id = i + 1
   begin
@@ -51,12 +44,7 @@ cb = ::Cutter::CircuitBreaker.new(threshold: 2, timeout: 1)
     puts "Request #{i+1} successful, with status code #{result.code}"
 
     payload = JSON.parse(result.body)
-    product = Product.new(title: payload["title"], description: payload["description"], category: payload["category"], price: payload["price"])
-    if product.save
-      puts "Product with id #{product.id} successfully created"
-    else
-      puts "Product with request #{i+1} failed to created"
-    end
+    datas << { id: payload["id"], title: payload["title"], description: payload["description"], category: payload["category"], price: payload["price"] }
   rescue => e
     puts "Request #{i+1} failed, with message #{e.message}"
   end
@@ -64,10 +52,10 @@ cb = ::Cutter::CircuitBreaker.new(threshold: 2, timeout: 1)
 end
 
 puts "\n"
-puts "============= [ all product data ] ================="
-puts "#{Product.all.to_a}"
-puts "============= [ all product data ] ================="
+puts "============= [ data that was successfully obtained ] ================="
+puts "#{datas}"
 puts "\n"
 
-puts "======================== ENDED [ case of targeted API performance in problematic conditions ] ========================"
-puts "\n"
+# puts "======================== STARTED [ case of adding new data ] ========================"
+# cb = ::Cutter::CircuitBreaker.new(threshold: 2, timeout: 1)
+
